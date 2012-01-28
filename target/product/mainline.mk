b08c05fef38ef31442f6f29abb6566ab967dd982
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-01-28 09:05:16 +0700

    Disable dataroaming by default
    The property is removed so that the property in vendor/arrow
    can take effect.
    
    Change-Id: Iefadc1a6e71cbf92ecb7363c953f4b5a82064446

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -33,10 +33,8 @@ endif
 ifeq ($(ARROW_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
-endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true \
+endif
 
 PRODUCT_PACKAGES += \
     PhotoTable \
