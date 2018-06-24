d871dd2e6fd83c036eed9ac9a92dbf4be09633d8
Luca Stefani <luca020400@lineageos.org>
2018-06-24 15:28:33 +0200

    build: Kill schedtool
    Change-Id: I07ea9b93824f23c19b7b912eb2226639c3532774

--- envsetup.sh
+++ envsetup.sh
@@ -1470,14 +1470,7 @@ function _complete_android_module_names() {
 
 # Make using all available CPUs
 function mka() {
-    case `uname -s` in
-        Darwin)
-            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
-            ;;
-        *)
-            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
-            ;;
-    esac
+    m -j "$@"
 }
 
 # Print colored exit condition
