b879d02c359dd6aab5dc7464b14129c1e5aa9f06
Ting Yu <ting.x.yu@sony.com>
2018-10-10 13:51:53 +0800

    Phone ringtone setting for Multi SIM device
    "ro.config.ringtone" can support Multi SIM device by spliting the
    ringtone for each slot with ",". So add description for it.
    
    Bug: 118735436
    Test: Manual
    Change-Id: I298024f500aa839ebfc15a3ee51e2f452c23b290

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -42,10 +42,11 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 ifeq ($(ARROW_BUILD),)
-# Additional settings used in all AOSP builds
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
+#   ro.config.ringtone supports Multi SIM Device configuration.
+#   Vendors can customize it by spliting the ringtone file for each slot with ",".
+#PRODUCT_PROPERTY_OVERRIDES := \
+#    ro.config.ringtone=Ring_Synth_04.ogg \
+#    ro.config.notification_sound=pixiedust.ogg
 endif
 
 # Put en_US first in the list, so make it default.
