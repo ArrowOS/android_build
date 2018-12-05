726f4953918aac7b4776f6725933bb503f88b01f
LuK1337 <priv.luk@gmail.com>
2018-12-05 20:51:49 +0100

    build: Force system-as-root layout for backuptool
    * Every device running Android 10 runs system-as-root by default
    
    Change-Id: If9f0eb58fb693a32b16a2036898b7986d8c1d953

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -956,7 +956,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if OPTIONS.backuptool:
     script.Mount("/system")
     script.Print("BackupTools: starting backup script")
-    script.RunBackup("backup")
+    script.RunBackup("backup", "/system/system")
     script.Print("BackupTools: DONE! Now real installation will begin")
     script.Unmount("/system")
 
@@ -1023,7 +1023,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.ShowProgress(0.02, 10)
     script.Mount("/system")
     script.Print("BackupTools: Restoring backup")
-    script.RunBackup("restore")
+    script.RunBackup("restore", "/system/system")
     script.Print("BackupTools: DONE!")
     script.Unmount("/system")
 
