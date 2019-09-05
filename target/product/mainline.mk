50259e195753051ea13c46bd8d0db28b128ae1a5
dianlujitao <dianlujitao@lineageos.org>
2019-09-05 21:56:36 +0800

    product: Exclude AOSP apn list on Arrow build
    Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -40,4 +40,6 @@ endif
 PRODUCT_PACKAGES += \
     PhotoTable
 
+ifeq ($(ARROW_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
