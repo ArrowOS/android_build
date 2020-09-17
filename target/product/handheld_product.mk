895a701f4204ccf623e417c91c44649ca676b82e
Bauuuuu <abansode@outlook.com>
2020-09-17 22:32:45 +0200

    handheld_product: Remove some packages
    Change-Id: I625f378a0777c777b20d89436bec6654f97fca85

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -22,7 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 
 # /product packages
 PRODUCT_PACKAGES += \
-    Browser2 \
     Calendar \
     Camera2 \
     Contacts \
@@ -32,7 +31,6 @@ PRODUCT_PACKAGES += \
     Music \
     OneTimeInitializer \
     preinstalled-packages-platform-handheld-product.xml \
-    QuickSearchBox \
     SettingsIntelligence \
     frameworks-base-overlays
 
