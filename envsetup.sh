a8a8b932817619c2700e339f316ab31809be782c
Bauuuuu <abansode@outlook.com>
2020-09-23 13:56:56 +0200

    envsetup: remove ccache compress config
    somehow setting here it does not work, as well
    ccache getting filled as new on every compilation
    without making use of it
    
    Change-Id: I1ed7601f0ac4eb4870ba439b5ecfc6404b78e876

--- envsetup.sh
+++ envsetup.sh
@@ -1633,7 +1633,6 @@ if [ -z ${CCACHE_EXEC} ]; then
     ccache_path=$(which ccache)
     if [ ! -z "$ccache_path" ]; then
 	export USE_CCACHE=1
-	export CCACHE_COMPRESS=1
         export CCACHE_EXEC="$ccache_path"
 	echo -e "\e[1mccache enabled and \e[32m\e[4mCCACHE_EXEC\e[0m \e[1mhas been set to : \e[4m$ccache_path\e[0m"
     else
