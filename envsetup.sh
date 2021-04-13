bf5bf58a80afa26658ba32949ca60eca219f15de
LuK1337 <priv.luk@gmail.com>
2021-04-13 19:57:24 +0200

    envsetup: Fix the way we check check_product's return value
    Change-Id: I5a2a4b7be5dfee198094902aaa2c9a8c27b97704

--- envsetup.sh
+++ envsetup.sh
@@ -655,8 +655,7 @@ function lunch()
         return 1
     fi
 
-    check_product $product
-    if [ $? -ne 0 ]
+    if ! check_product $product
     then
         # if we can't find a product, try to grab it off the ArrowGitHub
         T=$(gettop)
